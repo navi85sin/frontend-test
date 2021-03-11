79d90726657eacb74509534a55ce1a225c8e33f4
nav <navi85sin@gmail.com>
2021-03-11 13:53:48 +0000

    testing api changes

--- src/Autocomplete.js
+++ src/Autocomplete.js
@@ -12,24 +12,34 @@ const Autocomplete = (props) => {
   const [searchTerm, setSearchTerm] = useState("");
   const [suggestions, setSuggestions] = useState([]);
   const [searchTermValue] = useDebounce(searchTerm, 500);
+    
+  const fetchApiCall = async () => {
+    try{
+    await fetchSuggestions(searchTerm).then((_suggestions) =>
+      setSuggestions(_suggestions)
+    );
+    } catch (e) {
 
-  useEffect(() => {
-    const fetchApiCall = async () => {
-      await fetchSuggestions(searchTermValue).then((_suggestions) =>
-        setSuggestions(_suggestions)
-      );
-    };
-    if (searchTermValue) {
-      fetchApiCall();
     }
-  }, [searchTermValue]);
+  }; 
+  /*
+  const changeText = (val) => {
+    setSearchTerm(val);
+    setSuggestions([{id:5, title:val}, {id:3, title:'new val'}]);
+  }*/
+  useEffect(() => {
+    if(searchTerm){
+      fetchApiCall();
+      //setSuggestions([{id:5, title:searchTerm}, {id:3, title:'new val'}]);
+    }
+
+  }, [searchTerm]);
 
   // Onclick function for search items
-  const productSelect = (e, id) => {
-    setSearchTerm("");
-    setSuggestions("");
-    // Set value to parent component
+  const productSelect = (id) => {
+    //console.log(id);
     props.getProductID(id);
+    setSearchTerm('');
   };
 
   return (
@@ -43,8 +53,8 @@ const Autocomplete = (props) => {
       />
       {!!suggestions ? (
         <ul className="suggestionsList">
-          {suggestions.slice(0, suggestionArraySize).map((item) => (
-            <li key={item.id} onClick={(e) => productSelect(e, item.id)}>
+          {suggestions.map((item) => (
+            <li key={item.id} data-testid={item.id}  onClick={(e) => productSelect(item.id)}>
               {item.title}
             </li>
           ))}
--- src/Autocomplete.test.js
+++ src/Autocomplete.test.js
@@ -1,42 +1,82 @@
 import * as React from "react";
-import Enzyme from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
-import { mount } from "enzyme";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor, fireEvent, getByText } from "@testing-library/react";
+//import userEvent from '@testing-library/user-event'
+//import { renderHook, act } from "@testing-library/react-hooks";
+import {act} from 'react-dom/test-utils';
 import Autocomplete from "./Autocomplete";
+//import { fetchSuggestions } from "./utils/api";
+ //jest.mock("./utils/api");
+ 
+//import { fetchSuggestions } from "./utils/api";
 
-Enzyme.configure({ adapter: new Adapter() });
-jest.mock("./utils/api");
 
 describe("Autocomplete", () => {
-  beforeEach(() => {});
+  beforeEach(() => {
+    //jest.useFakeTimers()
+  });
 
   afterEach(() => {
     jest.clearAllMocks();
   });
 
-  it("renders correctly", () => {
-    render(<Autocomplete />);
-    const input = screen.getByRole("textbox", {
-      placeholder: /search for a product/i,
-    });
+  it("render the input button",  async () => {
+    let clic = jest.fn();
+    const {getByText, getByPlaceholderText, getAllByTestId} = render(<Autocomplete getProductID ={clic} />);
+    const input = screen.getByPlaceholderText('Search for a product');
     expect(input).toBeInTheDocument();
-  });
+    expect(input.value).toBe('');
+    fireEvent.change(input, { target: { value: 'men' } });
+    // check if text of input button is updated
+    expect(input.value).toBe('men');
+    expect(clic.mock.calls.length).toBe(0);
+    await waitFor(() => {
+     // await ( async() => {
+        const li = screen.getByText('Mens Cotton Jacket');      
+      })
 
-  it("input text box is empty, Autocomplete List is empty", () => {
-    const wrapper = mount(<Autocomplete />);
-    const input = wrapper.find(".search-box");
-    expect(input.get(0).props.value).toEqual("");
-    expect(wrapper.find(".suggestionsList").exists()).toBeTruthy();
-    expect(wrapper.find(".suggestionsList").children().exists()).toBe(false);
+		//})
+    //screen.debug(api);
+    // click the list item
+    fireEvent.click(screen.getByText('Mens Cotton Jacket'));
+    // check if props function getProductID is being called or not
+    expect(clic.mock.calls.length).toBe(1);
+    expect(input.value).toBe('');
   });
+  
+  // using the mock API 
+  /*
+  it("render the input button",  async () => {
+     
+   let data  = [
+     {
+    "id": 2,
+    "title": "Mens Casual Premium Slim Fit T-Shirts "
+      },
+      {
+        "id": 3,
+        "title": "Mens Cotton Jacket"
+      }
+    ];
+    fetchSuggestions.mockResolvedValue({data});
 
-  it("Search box text gets updated", () => {
-    const wrapper = mount(<Autocomplete />);
-    wrapper
-      .find(".search-box")
-      .simulate("change", { target: { value: "Men" } });
-    wrapper.update();
-    expect(wrapper.find(".search-box").props().value).toEqual("Men");
-  });
+    let clic = jest.fn();
+    
+    const {getByText, getByPlaceholderText, getAllByTestId} = render(<Autocomplete getProductID ={clic} />);
+
+    const input = screen.getByPlaceholderText('Search for a product');
+    
+    //act(() => jest.advanceTimersByTime(1000));
+    //await act( async () => {
+      act(() => {
+      fireEvent.change(input, { target: { value: 'men' } });
+     });
+      //fireEvent.click(screen.getByText('Mens Cotton Jacket'));
+      // expect(input.value).toBe('');
+
+      screen.debug();
+
+      });
+*/
+
+   
 });
